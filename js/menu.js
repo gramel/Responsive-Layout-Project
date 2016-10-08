5c4487ff32c9df55d1322467d70f9afaf49c9166
gramel <gramel@mail.com>
2016-10-08 23:59:58 +0900

    Revert "Add first project files"
    This reverts commit 270abc2e70907ad9f4d13b0b62ebd97f5d123d19.

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-/* Menu Functionality
-–––––––––––––––––––––––––––––––––––––––––––––––––– */
-
-//Trigger mavigation when menu is clicked
-$(".menu").click(function(){
-  $(".main-nav").toggle("slow");
-  event.preventDefault();
-});
